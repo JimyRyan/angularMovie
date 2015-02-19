be1c9b436c67dc3dd4cd50163c69e353a6a35f0c
G. Mocquet <gmocquet@gmail.com>
2015-02-19 13:54:40 +0000

    done

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,1 +1,15 @@
 
+
+angularMovieApp.filter('checkPoster', function () {
+    return function(inputValue) {
+
+        var o = inputValue;
+
+        if (!inputValue)
+        {
+            o = 'img/no-poster.jpg';
+        }
+
+        return o;
+    };
+});
